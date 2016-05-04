a297ce422c1d396e61754189f20917775e0fb0b6
Haythem Hammour <hammour@gmail.com>
2016-05-04 03:10:27 -0500

    random commit

--- routes/api1.js
+++ routes/api1.js
@@ -2,13 +2,10 @@ let express = require('express');
 let router = express.Router();
 let path = require('path');
 let bookshelfApi = require('bookshelf-api')({
-	path: path.join(__dirname,'..','models')
+	path: path.join(__dirname, '..','models')
 });
-router.use('/', bookshelfApi);
-
-
-// router.get('/products', function(req, res, next) {
-
-// });
+router.use('/', 
+	bookshelfApi
+);
 
 module.exports = router;
